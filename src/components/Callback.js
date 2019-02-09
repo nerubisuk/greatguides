4ab89e5588dae2d5845205c855559f204330a02a
Roman K <roman@obraz.es>
2019-02-09 23:17:31 +0200

    Adds <Callback> component

---
+++ src/components/Callback.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Icon from './Icon';
+
+class Callback extends React.Component {
+  render() {
+    const style = {
+      position: 'absolute',
+      display: 'flex',
+      justifyContent: 'center',
+      height: '100vh',
+      width: '100vw',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'white'
+    };
+
+    return (
+      <div style={style}>
+        <Icon name='loader' />
+      </div>
+    );
+  }
+}
+
+export default Callback;
