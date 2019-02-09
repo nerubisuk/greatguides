3c4d92ef2ab2aeab6e0ee39205b3494cd0393f6b
Roman K <roman@obraz.es>
2019-02-10 01:51:48 +0200

    Injects <Footer> to main layout

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -5,11 +5,13 @@
  */
 import React from 'react';
 import Header from 'components/Header';
+import Footer from 'components/Footer';
 
 /* Component definition */
 const Main = ({ ...props }) =>
   <>
     <Header {...props} />
+    <Footer />
   </>;
 
 export default Main;
