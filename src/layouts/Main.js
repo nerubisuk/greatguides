90a2a728cbcb32d4994aa828ae57f0d79c672cd7
Roman K <roman@obraz.es>
2019-02-10 08:43:33 +0200

    Adds <Adventure> page to layout

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -6,11 +6,13 @@
 import React from 'react';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import Adventure from 'pages/Adventure';
 
 /* Component definition */
 const Main = ({ ...props }) =>
   <>
     <Header {...props} />
+    <Adventure />
     <Footer />
   </>;
 
