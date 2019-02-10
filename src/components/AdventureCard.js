afbbea3c5535a5967d992584331f5c8e676aa67f
Roman K <roman@obraz.es>
2019-02-10 08:42:59 +0200

    Adds <AdventureCard> component

---
+++ src/components/AdventureCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Icon from 'components/Icon';
+import styles from 'styles/components/AdventureCard.module.scss';
+
+const AdventureCard = ({ adventure }) =>
+  <div className={styles.wrapper}>
+    <div className={styles.thumb}>
+      <div className={styles.price}>${adventure.price}</div>
+      <div className={styles.like}><Icon name='heart' /></div>
+
+      <div className={styles.guide}>
+        <div className={styles.face}>
+          <img src={adventure.avatar} />
+        </div>
+        <p>{adventure.guide}</p>
+        <div className={styles.info}>
+          <Icon name="star" />
+          <span>{adventure.ratings.substring(0, 3)}</span>
+          <span>&bull;</span>
+          <span>{adventure.reviews} reviews</span>
+        </div>
+      </div>
+
+      <img src={adventure.thumbnail} alt="thumbnail" />
+    </div>
+
+    <div className={styles.description}>
+      <div className={styles.type}>{adventure.type}</div>
+      <p>{adventure.title}</p>
+      <div className={styles.location}>
+        <Icon name='location' />
+        {adventure.location}
+      </div>
+    </div>
+  </div>;
+
+
+export default AdventureCard;
