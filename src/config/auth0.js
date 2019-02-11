78eb59c60e14a00d5d90f1a32739481f2af7d054
Roman K <roman@obraz.es>
2019-02-11 06:51:33 +0200

    Removes unusual code

--- src/config/auth0.js
+++ src/config/auth0.js
@@ -1,69 +1,31 @@
-// TODO: REWRITE THAT SHAME BELOW
+/* Get environment variable */
+const env = process.env
 
-export function environment() {
-  return 'development';
-  // return 'production'
+let authConfig
+
+switch (env) {
+  case 'production':
+    authConfig = {
+      AUTH_CONFIG: {
+        domain: 'actively-me.auth0.com',
+        clientId: 'OpXwYlLnBcwXCOn3vu730Z9RUoXc8Bot',
+        siteUrl: 'http://greatguides-dev.s3-website-us-west-1.amazonaws.com/',
+        logoutUrl: 'https://actively-me.auth0.com/v2/logout',
+        callbackUrl: 'http://greatguides-dev.s3-website-us-west-1.amazonaws.com/callback',
+      },
+    }
+    break
+
+  default:
+    authConfig = {
+      AUTH_CONFIG: {
+        domain: 'activelyme.auth0.com',
+        clientId: 'N7kW1H0nDWrlwxTIwg1Fbb2RHggHlcUB',
+        siteUrl: 'http://localhost:3000',
+        logoutUrl: 'https://activelyme.auth0.com/v2/logout',
+        callbackUrl: 'http://localhost:3000/callback',
+      },
+    }
 }
 
-
-export function getAuth0Object() {
-  const env = environment();
-  if (env === 'production') {
-    return {
-      'AUTH_CONFIG':{
-        'domain': 'actively-me.auth0.com',
-        'clientId': 'OpXwYlLnBcwXCOn3vu730Z9RUoXc8Bot',
-        'siteUrl': 'https://www.greatguides.com',
-        'logoutUrl': 'https://actively-me.auth0.com/v2/logout',
-        'callbackUrl': 'https://www.greatguides.com/callback',
-      }
-    };
-  }else if (env === 'staging') {
-    return {
-      'AUTH_CONFIG':{
-        'domain': 'activelyme.auth0.com',
-        'clientId': 'cAmdsZSi8R6G8k6j4V5POyxpoueFEVbA',
-        'siteUrl': 'https://activelyme-cd63e.firebaseapp.com',
-        'logoutUrl': 'https://activelyme.auth0.com/v2/logout',
-        'callbackUrl': 'https://activelyme-cd63e.firebaseapp.com/callback',
-      }
-    };
-  }else if (env === 'awsStaging') {
-    return {
-      'AUTH_CONFIG':{
-        'domain': 'activelyme.auth0.com',
-        'clientId': 'uAzm5XvojLtys2D28fgddhLkzsjiir1o',
-        'siteUrl': 'http://activelyme-test-code.s3-website-us-east-1.amazonaws.com',
-        'logoutUrl': 'https://activelyme.auth0.com/v2/logout',
-        'callbackUrl': 'http://activelyme-test-code.s3-website-us-east-1.amazonaws.com/callback',
-      }
-    };
-  }else if (env === 'development') {
-    return {
-      'AUTH_CONFIG':{
-        'domain': 'activelyme.auth0.com',
-        'clientId': 'N7kW1H0nDWrlwxTIwg1Fbb2RHggHlcUB',
-        'siteUrl': 'http://localhost:3000',
-        'logoutUrl': 'https://activelyme.auth0.com/v2/logout',
-        'callbackUrl': 'http://localhost:3000/callback',
-      }
-    };
-  }}
-
-export function getActivelymeCoreEnvUrl() {
-  const env = environment();
-  if (env === 'production') {
-    return 'https://api.services.activelyme.com/';
-  } else {
-    return 'https://api-test.services.activelyme.com/';
-  }
-}
-
-export function getActivelymePlaygroundEnvUrl() {
-  const env = environment();
-  if (env === 'production') {
-    return 'https://api.services.activelyme.com/graphql';
-  } else {
-    return 'https://api-test.services.activelyme.com/graphql';
-  }
-}
+export default authConfig
