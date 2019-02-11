a1dcab5627d264ab7a8a6ae618f6480db3e5fe5b
Roman K <roman@obraz.es>
2019-02-11 10:10:42 +0200

    Rewrites auth0 conf

--- src/config/auth0.js
+++ src/config/auth0.js
@@ -1,31 +1,31 @@
 /* Get environment variable */
-const env = process.env
+const env = process.env;
 
-let authConfig
+let authConfig;
 
 switch (env) {
-  case 'production':
-    authConfig = {
-      AUTH_CONFIG: {
-        domain: 'actively-me.auth0.com',
-        clientId: 'OpXwYlLnBcwXCOn3vu730Z9RUoXc8Bot',
-        siteUrl: 'http://greatguides-dev.s3-website-us-west-1.amazonaws.com/',
-        logoutUrl: 'https://actively-me.auth0.com/v2/logout',
-        callbackUrl: 'http://greatguides-dev.s3-website-us-west-1.amazonaws.com/callback',
-      },
-    }
-    break
+case 'production':
+  authConfig = {
+    AUTH_CONFIG: {
+      domain: 'actively-me.auth0.com',
+      clientId: 'OpXwYlLnBcwXCOn3vu730Z9RUoXc8Bot',
+      siteUrl: 'http://greatguides-dev.s3-website-us-west-1.amazonaws.com/',
+      logoutUrl: 'https://actively-me.auth0.com/v2/logout',
+      callbackUrl: 'http://greatguides-dev.s3-website-us-west-1.amazonaws.com/callback',
+    },
+  };
+  break;
 
-  default:
-    authConfig = {
-      AUTH_CONFIG: {
-        domain: 'activelyme.auth0.com',
-        clientId: 'N7kW1H0nDWrlwxTIwg1Fbb2RHggHlcUB',
-        siteUrl: 'http://localhost:3000',
-        logoutUrl: 'https://activelyme.auth0.com/v2/logout',
-        callbackUrl: 'http://localhost:3000/callback',
-      },
-    }
+default:
+  authConfig = {
+    AUTH_CONFIG: {
+      domain: 'activelyme.auth0.com',
+      clientId: 'N7kW1H0nDWrlwxTIwg1Fbb2RHggHlcUB',
+      siteUrl: 'http://localhost:3000',
+      logoutUrl: 'https://activelyme.auth0.com/v2/logout',
+      callbackUrl: 'http://localhost:3000/callback',
+    },
+  };
 }
 
-export default authConfig
+export default authConfig;
