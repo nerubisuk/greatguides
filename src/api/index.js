2ea46403c03eeb92e832d37d97aac9c4aacdf635
Roman K <roman@obraz.es>
2019-02-18 02:56:44 +0200

    Adds authorization handling to Apollo Client

--- src/api/index.js
+++ src/api/index.js
@@ -10,15 +10,16 @@ import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { WebSocketLink } from 'apollo-link-ws';
 import { getMainDefinition } from 'apollo-utilities';
+import auth from 'utils/auth';
 
 // Get the authentication token from the local storage
 const authorization = () => {
-  const token = localStorage.getItem('');
+  const token = auth.getIdToken();
   return token ? `Bearer ${token}` : '';
 };
 
 const httpLink = createHttpLink({
-  uri: '',
+  uri: 'https://api-test.services.activelyme.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -26,17 +27,17 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization,
+      authorization: authorization(),
     },
   };
 });
 
 const wsLink = new WebSocketLink({
-  uri: '',
+  uri: 'wss://us1.prisma.sh/info/activelyme/dev',
   options: {
     reconnect: true,
     connectionParams: {
-      authorization,
+      authorization: authorization(),
     },
   },
 });
