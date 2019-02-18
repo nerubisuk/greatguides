6f151406ea829cc0776d3bec95c1bbeb84fd72d8
Roman K <roman@obraz.es>
2019-02-18 17:14:19 +0200

    Adds basic reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,26 @@
-import { combineReducers } from 'redux';
+/**
+ * @file Holds the Redux reducers
+ * @since 0.1.0
+ * @author Anton Komarenko <mi3ta@sent.as>
+ */
+import { handleActions } from 'redux-actions';
+import initialState from './state';
+import { _LOGGER_ } from 'utils/helpers-common';
 
-const rootReducer = combineReducers({});
+export default handleActions(
+  {
+    ADVENTURE_BY_ID_SUCCEEDED: (state, { adventure }) => {
+      if (!state.adventures.some(item => item.id === adventure.id)) {
+        state.adventures.push(adventure);
+      }
 
-export default rootReducer;
+      _LOGGER_('ADVENTURE_BY_ID_SUCCEEDED');
+
+      return {
+        ...state,
+      };
+    },
+  },
+
+  initialState
+);
