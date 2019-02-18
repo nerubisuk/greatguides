880f0cd332f87dc8eb7e8fe76c69a32597673803
Roman K <roman@obraz.es>
2019-02-18 17:15:55 +0200

    Adds root array of sagas

---
+++ src/store/sagas/index.js
@@ -0,0 +1,11 @@
+/**
+ * @file Holds the root sagas array
+ * @since 0.1.0
+ * @author Anton Komarenko <mi3ta@sent.as>
+ */
+import fetchAdventureById from './fetchAdventureById';
+
+export default [
+  // Adventures
+  fetchAdventureById,
+];
