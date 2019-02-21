e89345ed1069c51ab1abd96a05113b937e7b93f6
Roman K <roman@obraz.es>
2019-02-21 07:56:35 +0200

    Adds more fields for getAdventureById query

--- src/api/queries.js
+++ src/api/queries.js
@@ -1,10 +1,41 @@
 import gql from 'graphql-tag';
 
-export const GetAdventureById = gql`
+export const getAdventureById = gql`
   query adventureById($id: ID) {
     adventure(where: { id: $id }) {
       id
       title
+      categories
+      description
+      address
+      price
+      reviews
+      inclusions
+      media {
+        imageUrl
+      }
+      guide {
+        id
+        country
+        city
+        profilePictureUrl
+        auth0ProfilePictureUrl
+        aboutMe
+        flagIcon
+        fullName
+        firstName
+        lastName
+        adventures {
+          id
+        }
+      }
+      durationInDays
+      distance
+      elevation
+      difficulty
+      inclusions
+      season
+      minimumAge
     }
   }
 `;
