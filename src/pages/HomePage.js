16ec99e5e405516b544c500645f26b5e112b70a2
Roman K <roman@obraz.es>
2019-02-21 07:51:44 +0200

    Adds some initial code for <HomePage> page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,22 @@
+/**
+ * @file Holds the <HomePage> page
+ * @since 0.1.0
+ * @author Anton Komarenko <mi3ta@sent.as>
+ */
+import React from 'react';
+import styles from 'styles/pages/HomePage.module.scss';
+import PopularAdventures from 'containers/PopularAdventures';
+import data from 'mocks/home.json';
+
+/* Component definition */
+class HomePage extends React.Component {
+  render() {
+    return (
+      <main className={styles.wrapper}>
+        <PopularAdventures adventures={data.popular} />
+      </main>
+    );
+  }
+}
+
+export default HomePage;
