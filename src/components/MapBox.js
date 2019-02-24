18b673a058a14a29c77e46f24c28f3d3e87255d9
Roman K <roman@obraz.es>
2019-02-25 01:59:15 +0200

    Adds basic <MapBox> component

---
+++ src/components/MapBox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from 'styles/components/MapBox.module.scss';
+
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import config from 'config/mapbox';
+mapboxgl.accessToken = config.accessToken;
+
+class MapBox extends React.Component {
+  state = {
+    map: null,
+  };
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: 'mapBoxContainer',
+      style: 'mapbox://styles/onelastjedi/cjsjdze6e3m331fny2b6hk595',
+      center: [-75.911136, 42.097267],
+      zoom: 14.1,
+    });
+
+    this.setState({
+      map,
+    });
+  }
+
+  render() {
+    return <div className={styles.wrapper} id='mapBoxContainer' />;
+  }
+}
+
+export default MapBox;
