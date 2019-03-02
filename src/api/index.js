5670d2a811013b078ce083e6b6fe690d4f58b1fd
Roman K <roman@obraz.es>
2019-03-02 04:23:57 +0200

    Removes redundant definitions from Apollo client setup

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,9 @@
  * @author Anton Komarenko <mi3ta@sent.as>
  */
 import { ApolloClient } from 'apollo-client';
-import { split } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { WebSocketLink } from 'apollo-link-ws';
-import { getMainDefinition } from 'apollo-utilities';
 import auth from 'utils/auth';
 
 const { REACT_APP_GRAPHQL_API_URL } = process.env;
@@ -36,28 +33,13 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-/* Creates the wsLink */
-const wsLink = new WebSocketLink({
-  uri: 'wss://us1.prisma.sh/info/activelyme/dev',
-  options: {
-    reconnect: true,
-    connectionParams: {
-      authorization: authorization(),
-    },
-  },
-});
-
 /* Defines the main link for Apollo client */
-const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
-  },
-  wsLink,
-  authLink.concat(httpLink)
-);
+const link = authLink.concat(httpLink);
+
+/* Defines the cache */
+const cache = new InMemoryCache();
 
 export default new ApolloClient({
   link,
-  cache: new InMemoryCache(),
+  cache,
 });
