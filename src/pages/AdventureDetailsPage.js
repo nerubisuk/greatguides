a3f3b216e77224f482e8d5e0ad437c2e7e062f3d
Roman K <roman@obraz.es>
2019-03-02 04:26:15 +0200

    Refactors <AdventureDetailsPage> using no-redux approach

--- src/pages/AdventureDetailsPage.js
+++ src/pages/AdventureDetailsPage.js
@@ -4,6 +4,7 @@
  * @author Anton Komarenko <mi3ta@sent.as>
  */
 import React from 'react';
+import { graphql } from 'react-apollo';
 import Icon from 'components/Icon';
 import PhotoSlider from 'components/PhotoSlider';
 import AdventureCard from 'components/AdventureCard';
@@ -12,147 +13,135 @@ import TourDescription from 'components/TourDescription';
 import CarryThings from 'components/CarryThings';
 import BookRequest from 'components/BookRequest';
 import TourOnTheMap from 'components/TourOnTheMap';
+import ReviewsList from 'components/ReviewsList';
 import PropTypes from 'prop-types';
 import styles from 'styles/pages/AdventureDetailsPage.module.scss';
-import { connect } from 'react-redux';
-import { getAdventureById } from 'store/actions';
-import data from 'mocks/adventures';
+import { getAdventureById } from 'api/queries';
+import mockData from 'mocks/adventures';
 
 /* Component definition */
-class AdventureDetailsPage extends React.Component {
-  componentDidMount() {
-    this.props.getAdventureById({
-      id: 'cjn05y8bfq7el0b48w4mleu8x',
-    });
-  }
+const PureAdventureDetailsPage = ({ data, mql }) => {
+  const { loading, adventure } = data;
 
-  render() {
-    const { adventure, mql } = this.props;
+  if (loading) return 'Loading...';
 
-    return (
-      <main className={styles.wrapper}>
-        {adventure && (
-          <React.Fragment>
-            {/* Header */}
-            <div className={styles.header}>
-              <section>
-                <div className={styles.type}>{adventure.categories}</div>
-                <h1>{adventure.title}</h1>
-                <div className={styles.location}>
-                  <Icon name='location' />
-                  {adventure.address}
-                </div>
-              </section>
-              <section>
-                <hr />
-                <div className={styles.price}>
-                  ${adventure.price}&nbsp;
-                  <span>tour price</span>
-                </div>
-                <div className={styles.ratings}>
-                  {[0, 0, 0, 0, 0].map((item, index) => (
-                    <Icon key={index} name='star' />
-                  ))}
-                  <span>{adventure.reviews}</span>
-                </div>
-              </section>
+  return (
+    <main className={styles.wrapper}>
+      <React.Fragment>
+        {/* Header */}
+        <div className={styles.header}>
+          <section>
+            <div className={styles.type}>{adventure.categories}</div>
+            <h1>{adventure.title}</h1>
+            <div className={styles.location}>
+              <Icon name='location' />
+              {adventure.address}
             </div>
+          </section>
+          <section>
+            <hr />
+            <div className={styles.price}>
+              ${adventure.price}&nbsp;
+              <span>tour price</span>
+            </div>
+            <div className={styles.ratings}>
+              {[0, 0, 0, 0, 0].map((item, index) => (
+                <Icon key={index} name='star' />
+              ))}
+              <span>{adventure.reviews}</span>
+            </div>
+          </section>
+        </div>
 
-            {/* PhotoSlider */}
-            <PhotoSlider photos={adventure.media} />
+        {/* PhotoSlider */}
+        <PhotoSlider photos={adventure.media} />
 
-            <div className={styles.row}>
-              <div className={styles.column}>
-                {/* Departure dates */}
-                <DepartureDates dates={data[0].dates} />
+        <div className={styles.row}>
+          <div className={styles.column}>
+            {/* Departure dates */}
+            <DepartureDates dates={mockData[0].dates} />
 
-                {/* Tour description */}
-                <TourDescription
-                  description={adventure.description}
-                  specs={{
-                    distance: adventure.distance,
-                    duration: adventure.durationInDays,
-                    season: adventure.season,
-                    minimum_age: adventure.minimumAge,
-                  }}
-                />
+            {/* Tour description */}
+            <TourDescription
+              description={adventure.description}
+              specs={{
+                distance: adventure.distance,
+                duration: adventure.durationInDays,
+                season: adventure.season,
+                minimum_age: adventure.minimumAge,
+              }}
+            />
 
-                {/* Carry */}
-                <CarryThings carry={adventure.thingsToCarryList} />
+            {/* Carry */}
+            <CarryThings carry={adventure.thingsToCarryList} />
 
-                <div className={styles.col_1_2}>
-                  {/* Included */}
-                  <div className={styles.included_excluded}>
-                    <h3>Included</h3>
-                    <ul>
-                      {adventure.inclusionList.map((item, index) => (
-                        <li key={index}>
-                          <Icon name='plus' className={styles.icon_plus} />
-                          {item}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-
-                  {/* Excluded */}
-                  <div className={styles.included_excluded}>
-                    <h3>Excluded</h3>
-                    <ul>
-                      {adventure.exclusionList.map((item, index) => (
-                        <li key={index}>
-                          <Icon name='minus' className={styles.icon_minus} />
-                          {item}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-
-                {/* Tour on the map */}
-                <TourOnTheMap />
+            <div className={styles.col_1_2}>
+              {/* Included */}
+              <div className={styles.included_excluded}>
+                <h3>Included</h3>
+                <ul>
+                  {adventure.inclusionList.map((item, index) => (
+                    <li key={index}>
+                      <Icon name='plus' className={styles.icon_plus} />
+                      {item}
+                    </li>
+                  ))}
+                </ul>
               </div>
 
-              {mql === 'desktop' && (
-                <div className={styles.column}>
-                  <BookRequest guide={adventure.guide} />
-                </div>
-              )}
-            </div>
-
-            {/* Similar adventures */}
-            <div className={styles.similar}>
-              <h2>Similar Adventures</h2>
-              {
-                <section>
-                  {data[0].similar.map((item, index) => (
-                    <AdventureCard key={index} adventure={item} />
+              {/* Excluded */}
+              <div className={styles.included_excluded}>
+                <h3>Excluded</h3>
+                <ul>
+                  {adventure.exclusionList.map((item, index) => (
+                    <li key={index}>
+                      <Icon name='minus' className={styles.icon_minus} />
+                      {item}
+                    </li>
                   ))}
-                </section>
-              }
+                </ul>
+              </div>
             </div>
-          </React.Fragment>
-        )}
-      </main>
-    );
-  }
-}
 
-/* Prop types definition */
-AdventureDetailsPage.propTypes = {
-  adventure: PropTypes.object,
-  mql: PropTypes.string.isRequired,
-  getAdventureById: PropTypes.func,
+            {/* Tour on the map */}
+            <TourOnTheMap />
+
+            {/* Reviews */}
+            <ReviewsList reviews={mockData[0].reviews} />
+          </div>
+
+          {mql === 'desktop' && (
+            <div className={styles.column}>
+              <BookRequest guide={adventure.guide} />
+            </div>
+          )}
+        </div>
+
+        {/* Similar adventures */}
+        <div className={styles.similar}>
+          <h2>Similar Adventures</h2>
+          {
+            <section>
+              {mockData[0].similar.map((item, index) => (
+                <AdventureCard key={index} adventure={item} />
+              ))}
+            </section>
+          }
+        </div>
+      </React.Fragment>
+    </main>
+  );
 };
 
-/* Redux state mapping */
-const mapStateToProps = state => ({
-  adventure: state.adventures.find(item => item.id === 'cjn05y8bfq7el0b48w4mleu8x'),
-});
+/* Prop types definition */
+PureAdventureDetailsPage.propTypes = {
+  data: PropTypes.object,
+  mql: PropTypes.string.isRequired,
+  match: PropTypes.object,
+};
 
-/* Redux actions mapping */
-const mapDispatchToProps = { getAdventureById };
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AdventureDetailsPage);
+export default graphql(getAdventureById, {
+  options: ({ match }) => ({
+    variables: { id: match.params.id },
+  }),
+})(PureAdventureDetailsPage);
