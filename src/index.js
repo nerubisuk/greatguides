e532950667a686a8c198c1f7f9647950c4885472
Roman K <roman@obraz.es>
2019-03-07 16:37:32 +0200

    Added <StoreProvider> to the app entry point

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { StoreProvider } from './store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'styles/base.scss';
 
@@ -7,9 +8,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <StoreProvider>
+    <Router>
+      <App />
+    </Router>
+  </StoreProvider>,
   document.getElementById('root')
 );
 
