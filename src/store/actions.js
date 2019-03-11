9d99a4d40533e93a344b1bba8e557a378c0821fb
Roman K <roman@obraz.es>
2019-03-11 12:55:13 +0200

    Added actions using React.Context approach

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,16 +1,52 @@
 import API from 'api';
-import { getAdventureById } from 'api/queries';
+import { getAdventures, getGuides, getAdventureById } from 'api/queries';
 
-export const fetchAdventureById = async (id, dispatch) => {
+export const fetchGuides = async (first, dispatch) => {
+  try {
+    const { data } = await API.query({
+      query: getGuides,
+      variables: { first, role: 'Guide' },
+    });
+
+    return dispatch({
+      type: 'FETCH_GUIDES',
+      payload: data.users,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const fetchAdventures = async (first, dispatch) => {
+  try {
+    const { data } = await API.query({
+      query: getAdventures,
+      variables: { first },
+    });
+
+    return dispatch({
+      type: 'FETCH_ADVENTURES',
+      payload: data.adventures,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const fetchAdventureById = async (state, id, dispatch) => {
   try {
     const { data } = await API.query({
       query: getAdventureById,
       variables: { id },
     });
 
+    if (!state.adventures.some(item => item.id === data.adventure.id)) {
+      state.adventures.push(data.adventure);
+    }
+
     return dispatch({
       type: 'FETCH_ADVENTURE_BY_ID',
-      payload: data.adventure,
+      payload: state.adventures,
     });
   } catch (err) {
     console.error(err);
