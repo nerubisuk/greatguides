26973e2fd69aaf56543796ec6c5cb276cd5262c6
Roman K <roman@obraz.es>
2019-03-11 12:55:51 +0200

    Added 'getGuides' and 'getAdventures' queries

--- src/api/queries.js
+++ src/api/queries.js
@@ -1,5 +1,65 @@
 import gql from 'graphql-tag';
 
+export const getGuides = gql`
+  query users($first: Int, $role: UserRole) {
+    users(first: $first, where: { userRole: $role }) {
+      id
+      fullName
+      profile
+      profilePictureUrl
+      region
+      city
+      country
+      adventures {
+        id
+      }
+    }
+  }
+`;
+
+export const getAdventures = gql`
+  query adventures($first: Int) {
+    adventures(first: $first) {
+      id
+      title
+      dates
+      inclusionList
+      exclusionList
+      categories
+      thingsToCarryList
+      description
+      address
+      price
+      reviews
+      media {
+        id
+        imageUrl
+      }
+      guide {
+        id
+        country
+        city
+        profilePictureUrl
+        auth0ProfilePictureUrl
+        aboutMe
+        flagIcon
+        fullName
+        firstName
+        lastName
+        adventures {
+          id
+        }
+      }
+      durationInDays
+      distance
+      elevation
+      difficulty
+      season
+      minimumAge
+    }
+  }
+`;
+
 export const getAdventureById = gql`
   query adventureById($id: ID) {
     adventure(where: { id: $id }) {
@@ -15,6 +75,7 @@ export const getAdventureById = gql`
       price
       reviews
       media {
+        id
         imageUrl
       }
       guide {
